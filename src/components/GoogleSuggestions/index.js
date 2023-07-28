ec7de77575e2f49ba4c01f0353642995ba051b7c
SachinBalagam <balagamsachin337@gmail.com>
2023-07-28 16:58:50 +0000

    Final Commit

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,1 +1,62 @@
 // Write your code here
+import {Component} from 'react'
+import SuggestionItem from '../SuggestionItem'
+import './index.css'
+
+class GoogleSuggestions extends Component {
+  state = {searchInput: ''}
+
+  onInputChange = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  onClickButton = value => {
+    this.setState({searchInput: value})
+  }
+
+  render() {
+    const {suggestionsList} = this.props
+    const {searchInput} = this.state
+    const searchResults = suggestionsList.filter(each =>
+      each.suggestion.toLowerCase().includes(searchInput),
+    )
+    return (
+      <div className="bg-container">
+        <div className="card">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-logo.png "
+            alt="google logo"
+            className="google-logo"
+          />
+          <div className="search-list-container">
+            <div className="search-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+                alt="search icon"
+                className="search-icon"
+              />
+              <input
+                type="search"
+                className="input-bar"
+                placeholder="Search Google"
+                onChange={this.onInputChange}
+                value={searchInput}
+              />
+            </div>
+            <ul className="list-container">
+              {searchResults.map(each => (
+                <SuggestionItem
+                  suggestionsList={each}
+                  key={each.id}
+                  onClickButton={this.onClickButton}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,1 +1,26 @@
 // Write your code here
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestionsList, onClickButton} = props
+  const {suggestion} = suggestionsList
+
+  const onButtonClick = () => {
+    onClickButton(suggestion)
+  }
+
+  return (
+    <li className="each-list">
+      <p className="title">{suggestion}</p>
+      <button type="button" className="button" onClick={onButtonClick}>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+          className="arrow-image"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default SuggestionItem
